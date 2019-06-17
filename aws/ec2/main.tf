dd2ce7748724a702f4a20b61bacfba05b381081b
s-ari <sgt.arima@gmail.com>
2019-06-17 07:10:05 +0000

    Create security_group_rule module for AWS

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -29,6 +29,15 @@ module "subnet_az_a" {
   availability_zone = "ap-northeast-1a"
 }
 
+module "subnet_az_c" {
+  source            = "../modules/subnet"
+  vpc_id            = "${module.vpc.vpc_id}"
+  prefix            = "${var.prefix}"
+  cidr_block        = "192.168.2.0/24"
+  name              = "${var.prefix}_${terraform.workspace}_subnet_az_c"
+  availability_zone = "ap-northeast-1c"
+}
+
 module "igw" {
   source = "../modules/internet_gateway"
   vpc_id = "${module.vpc.vpc_id}"
@@ -40,14 +49,20 @@ module "route_table" {
   source     = "../modules/route_table"
   vpc_id     = "${module.vpc.vpc_id}"
   prefix     = "${var.prefix}"
-  name       = "${var.prefix}_${terraform.workspace}_rtable"
+  name       = "${var.prefix}_${terraform.workspace}_route_table"
   cidr_block = "0.0.0.0/0"
   gateway_id = "${module.igw.gateway_id}"
 }
 
-module "route_table_association" {
+module "route_table_association_az_a" {
   source         = "../modules/route_table_association"
   subnet_id      = "${module.subnet_az_a.subnet_id}"
+  route_table_id = "${module.route_table.route_table_id}"
+}
+
+module "route_table_association_az_c" {
+  source         = "../modules/route_table_association"
+  subnet_id      = "${module.subnet_az_c.subnet_id}"
   route_table_id = "${module.route_table.route_table_id}"
 }
 
@@ -63,10 +78,19 @@ module "security_group" {
   protocol    = "tcp"
 }
 
+module "security_group_rule" {
+  source      = "../modules/security_group_rule"
+  cidr_block  = "0.0.0.0/0"
+  from_port   = "80"
+  to_port     = "80"
+  protocol    = "tcp"
+  security_group_id = "${module.security_group.security_group_id}"
+}
+
 module "ec2" {
   source                      = "../modules/instance"
   prefix                      = "${var.prefix}"
-  name                        = "${var.prefix}_${terraform.workspace}_sg"
+  name                        = "${var.prefix}_${terraform.workspace}_ec2"
   key_name                    = "${var.ssh_key}"
   ami                         = "ami-09c81ecf1c2b2ef70"
   instance_type               = "t2.micro"
---
+++ aws/modules/security_group_rule/variable.tf
@@ -0,0 +1,19 @@
+variable "cidr_block" {
+  default = ""
+}
+
+variable "from_port" {
+  default = ""
+}
+
+variable "to_port" {
+  default = ""
+}
+
+variable "protocol" {
+  default = ""
+}
+
+variable "security_group_id" {
+  default = ""
+}
