9fe001d17c844cc79dc40a2687d5f1c4a95c279d
s-ari <sgt.arima@gmail.com>
2019-06-17 07:22:07 +0000

    Add egress access in security group for AWS

--- aws/modules/security_group/main.tf
+++ aws/modules/security_group/main.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = ["${var.cidr_block}"]
   }
 
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.name}"
   }
