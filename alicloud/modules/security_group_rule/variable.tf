33c19dbf1f2df7d78bacb0f698d393ad9448768a
s-ari <sgt.arima@gmail.com>
2019-06-24 05:07:56 +0000

    Create security_group_rule module for AliCloud

---
+++ alicloud/modules/security_group_rule/variable.tf
@@ -0,0 +1,31 @@
+variable "type" {
+  default = ""
+}
+
+variable "ip_protocol" {
+  default = ""
+}
+
+variable "port_range" {
+  default = ""
+}
+
+variable "nic_type" {
+  default = ""
+}
+
+variable "policy" {
+  default = ""
+}
+
+variable "port" {
+  default = ""
+}
+
+variable "cidr_ip" {
+  default = ""
+}
+
+variable "security_group_id" {
+  default = ""
+}
