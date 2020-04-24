64c278a42d1864853e42e6e18218a889aec4b0e9
s-ari <sgt.arima@gmail.com>
2020-04-24 15:45:24 +0900

    fix system_disk_category

--- alicloud/modules/ecs/main.tf
+++ alicloud/modules/ecs/main.tf
@@ -5,7 +5,7 @@ resource "alicloud_instance" "ecs" {
   instance_type              = "${var.instance_type}"
   instance_name              = "${var.instance_name}${format("%02d", count.index + 1)}"
   host_name                  = "${var.host_name}${format("%02d", count.index + 1)}"
-  system_disk_size           = "${var.system_disk_category}"
+  system_disk_category       = "${var.system_disk_category}"
   system_disk_size           = "${var.system_disk_size}"
   security_groups            = ["${var.security_groups}"]
   vswitch_id                 = "${var.vswitch_id}"
